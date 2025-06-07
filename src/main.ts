69adbf0bf856b6f4aa71d8e48ba3f7b863674ee1
Utsav Anand <utsavanand2@gmail.com>
2025-06-08 03:57:16 +0530

    Load WASM module

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import './style.css'
 import App from './App.vue'
 import routes from './router'
 import { i18n } from './i18n'
+import './wasm_exec.js'
 
 // Create the router instance
 const router = createRouter({
@@ -22,3 +23,17 @@ app.use(router)
 app.use(pinia)
 app.use(i18n)
 app.mount('#app')
+await loadWasm()
+
+async function loadWasm() {
+  if (!WebAssembly) {
+    throw new Error('WebAssembly is not supported in your browser')
+  }
+
+  const go = new window.Go()
+  const webAssemblyInstantiatedSrc = await WebAssembly.instantiateStreaming(
+    fetch('main.wasm'),
+    go.importObject,
+  )
+  await go.run(webAssemblyInstantiatedSrc.instance)
+}
