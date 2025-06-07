50edb8a0745f65ea451463f0cfa7c64f69c7bfe1
Utsav Anand <utsavanand2@gmail.com>
2025-06-08 03:57:56 +0530

    Add WASM function signature to the window interface

---
+++ src/global.d.ts
@@ -0,0 +1,17 @@
+export interface Hashes {
+  phash: string;
+  dhash: string;
+}
+
+export {};
+declare global {
+    export interface Window {
+        Go: {
+            new(): {
+                run: (inst: WebAssembly.Instance) => Promise<void>
+                importObject: WebAssembly.Imports
+            }
+        }
+        GetHashOfImage: (imageBuffer: Uint8Array<ArrayBuffer>, imageType: string) => Hashes
+    }
+}
